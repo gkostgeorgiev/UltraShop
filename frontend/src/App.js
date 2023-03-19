8b27988cee76e33704b91f48f1bf847baa7c77c1
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-03-19 23:27:18 +0200

    Updated the footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
 import Header from './components/Header.component';
 import Footer from './components/Footer.component';
 
@@ -7,7 +8,9 @@ function App() {
     <>
       <Header />
       <main>
+        <Container>
         <h1>Welcome to UltraShop</h1>
+        </Container>
       </main>
       <Footer />
     </>
--- frontend/src/components/Footer.component.js
+++ frontend/src/components/Footer.component.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 const Footer = () => {
-  return (
-    <footer>
-      Footer
-    </footer>
-  )
-}
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        Copyright &copy; UltraShop
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
