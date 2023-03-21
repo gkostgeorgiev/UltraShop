7300e7417afdfac8216fd87b5ad8ec6b6b4257a6
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-03-21 22:22:00 +0200

    Implemented custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 const router = express.Router();
 
 // @description  Fetch all products
-// @route GET /api/products 
+// @route GET /api/products
 // @access Public
 router.get(
   "/",
@@ -16,19 +16,19 @@ router.get(
 );
 
 // @description  Fetch  single product
-// @route GET /api/product/:id 
+// @route GET /api/product/:id
 // @access Public
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-   
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404).json({ message: 'Product not found'});
-    }
 
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
   })
 );
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-import productRoutes from './routes/productRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -13,7 +14,11 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
