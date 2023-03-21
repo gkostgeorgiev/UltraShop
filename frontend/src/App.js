1ae2c2d10bb7125c68d2461ddd84af0bcc06ff67
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-03-21 05:54:44 +0200

    Added React Router Dom
    Added Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Header from './components/Header.component';
-import Footer from './components/Footer.component';
-import HomeScreen from './screens/HomeScreen';
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.component";
+import Footer from "./components/Footer.component";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
+      <Fragment>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+        </Routes>
+      </Fragment>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header.component.js
+++ frontend/src/components/Header.component.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { Fragment } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+    <Fragment>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">UltraShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>UltraShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className='ms-auto'>
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </header>
+      <Outlet />
+    </Fragment>
   );
 };
 
