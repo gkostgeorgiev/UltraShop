09ce7c1996c01ccc73d69b2552bd68a4f81595be
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-04-05 15:30:03 +0300

    Implemented user registration on the backend
    Added functionality to hash user password on user creation.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -27,9 +27,22 @@ const userSchema = mongoose.Schema(
   }
 );
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-}
+};
+
+// this method will run on every .create() mongoose call
+userSchema.pre("save", async function (next) {
+  // mongoose will check first if the password has been modified/created
+  // if not, it will skip to the next middleware or save the data
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  // this.password pertains to the user object
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
