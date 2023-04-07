5004041e5c7e4c2dc06bd82851799aeb9c158def
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-04-07 13:08:35 +0300

    Updated profile endpoint on backend
    Added functionality to update user profile on the backend. Added a protected route to user routes for profile updates.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,9 @@ import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
-// @description Auth user & get a token
-// @route POST /api/users/login
-// @access Public
+// @description         Auth user & get a token
+// @route               POST /api/users/login
+// @access              Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -24,24 +24,24 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @description Register a new user
-// @route POST /api/users
-// @access Public
+// @description      Register a new user
+// @route            POST /api/users
+// @access           Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   const userExists = await User.findOne({ email });
 
   if (userExists) {
-    res.status(400)
-    throw new Error ('User already exists');
-  } 
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
   const user = await User.create({
     name,
     email,
-    password
-  })
+    password,
+  });
 
   if (user) {
     res.status(201).json({
@@ -50,17 +50,17 @@ const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
-    })
+    });
   } else {
-    res.status(400)
-    console.log('Invalid user data')
-    throw new Error('Invalid user data')
+    res.status(400);
+    console.log("Invalid user data");
+    throw new Error("Invalid user data");
   }
 });
 
-// @description get user profile
-// @route GET /api/users/profile
-// @access Private
+// @description     get user profile
+// @route           GET /api/users/profile
+// @access          Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -77,4 +77,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// @description   get user profile
+// @route         PUT /api/users/profile
+// @access        Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password){
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
