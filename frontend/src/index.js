e12fbf5c23cd17f07cca3f1ba79f4bac05ebcca0
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-04-20 09:04:21 +0300

    Preparing app for deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,9 @@ import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
